fbb795cb3336c34eb892f45eb9aa7c177c96cefd
catie <catie.lameo@gmail.com>
2026-05-17 13:01:22 +0700

    Thêm sprites WireTask

---
+++ Assets/Scripts/WireTask.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class WireTask : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
