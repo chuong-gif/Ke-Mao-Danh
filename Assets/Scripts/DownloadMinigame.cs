ba361b5d8ea17b9034f406675a42a41d0e196080
catie <catie.lameo@gmail.com>
2026-05-17 12:31:18 +0700

    Task download minigame - chỉnh sửa lại script

--- Assets/Scripts/DownloadMinigame.cs
+++ Assets/Scripts/DownloadMinigame.cs
@@ -1,1 +1,64 @@
-﻿
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DownloadMinigame : InteractableObject
+{
+    public float Download_Speed = 0.065f;
+    private Canvas DownloadUI;
+    private Slider SliderUi;
+    private bool IsFinished = false;
+
+    private void Awake()
+    {
+        ProgressTasks.TaskSetup();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        base.Initialize();
+
+        DownloadUI = GetComponentInChildren<Canvas>();
+        SliderUi = DownloadUI.GetComponentInChildren<Slider>();
+    }
+
+    public override void ExitAction()
+    {
+        base.ExitAction();
+        DownloadUI.enabled = false;
+        SliderUi.value = 0f;
+    }
+
+    public override void Interact()
+    {
+        base.Interact();
+        DownloadUI.enabled = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        base.CheckInteractionInput();
+
+
+        if (Using)
+        {
+            if (SliderUi.value < SliderUi.maxValue)
+            {
+                SliderUi.value += Download_Speed * Time.deltaTime;
+            }
+            else
+            {
+                IsFinished = true;
+                ExitAction();
+                SetCanInteract(false);
+                ProgressTasks.SetProgress(ProgressTasks.GetProgress() + ProgressTasks.GetDistributedValue());
+            }
+
+
+        }
+    }
+}
